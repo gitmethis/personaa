b94bd8984871431fd28db28ecebf4d8d30bb312f
sinclairstudy <sinclairstudy@gmail.com>
2018-04-19 16:40:53 -0400

    battles controller prob fixed

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -3,4 +3,14 @@ class BattlesController < ApplicationController
     @battles = Battle.all
     render json: @battles
   end
+
+  def create
+    @battle = Battle.create(battle_params)
+    render json: @battle
+  end
+
+  private
+  def battle_params
+    params.require(:battle).permit(:user1_id, :user2_id, :request, :user1_hp, :user2_hp)
+  end
 end
